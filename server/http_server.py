3db44e6580d26fb16feacc3b584faa2cbc794c61
Marius Asadauskas <marius.asadauskas@students.unibe.ch>
2022-12-18 18:36:02 +0100

    Undo button and sliding images

--- server/http_server.py
+++ server/http_server.py
@@ -2,27 +2,25 @@ import json
 import cv2
 import numpy as np
 import os
-
-import svgpathtools
 import toml
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect
 from svgpathtools import svg2paths, Path
 
 import pipeline.pipeline as pipeline
-from silhouette.Graphtec import SilhouetteCameo
 
-UPLOAD_FOLDER = './staticFiles/images'
+# from silhouette.Graphtec import SilhouetteCameo
+
+UPLOAD_FOLDER = './staticFiles/images/pngs/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+IMG_AMT = 4
 
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SECRET_KEY'] = 'the random string'
+app.config['SECRET_KEY'] = 'ProtoFabLab'
 config = toml.load('../config.toml')['Server']
 
-dev = SilhouetteCameo(dry_run=False)
-state = dev.status()  # hint at loading paper, if not ready.
-print(f'status={state}')
-print("device version: '%s'" % dev.get_version())
+# dev = SilhouetteCameo(dry_run=False)
+# state = dev.status()  # hint at loading paper, if not ready.
 
 with open('../pipeline/pipeline.json') as f:
     typ = dict[str, str | dict[str, str | list[str] | dict[str, int | float | str]]]
@@ -31,80 +29,6 @@ with open('../pipeline/pipeline.json') as f:
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-# TODO: Maybe this doesn't work correctly, I couldn't try yet with a frontend
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if request.form.getlist('undo'):
-            copy_backup()
-            return redirect(request.url)
-
-        if request.form.get('send-to-silhouette'):
-            send_to_silhouette()
-            return redirect(request.url)
-
-        blurs = request.form.getlist('blur') if request.form.getlist('blur') else None
-        threshold_type = request.form.get('threshold') if request.form.get('threshold') else None
-        invert = bool(request.form.get('invert')) if request.form.get('invert') else None
-        dilate_iterations = int(request.form.get('dilate')) if request.form.get('dilate') else None
-        erode_iterations = int(request.form.get('erode')) if request.form.get('erode') else None
-        contour_type = int(request.form.get('contour')) if request.form.get('contour') else None
-        cleanliness = float(request.form.get('clean')) / 10 if request.form.get('clean') else None
-        smoothness = float(request.form.get('smooth')) / 10 if request.form.get('smooth') else None
-
-        # check if the post request has the file part
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            # filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'main.png'))
-        root(blurs, threshold_type, invert, dilate_iterations, erode_iterations, contour_type, cleanliness, smoothness)
-        return redirect(request.url)
-    return render_template('index.html')
-
-
-def root(blurs, threshold_type: str, invert: bool, dilate_iterations: int, erode_iterations: int, contour_type: int,
-         cleanliness: float, smoothness: float):
-    img = cv2.imread('./staticFiles/images/main.png')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = gray
-
-    if blurs:
-        if 'median' in blurs:
-            blur = cv2.medianBlur(blur, 3)
-        if 'gaussian' in blurs:
-            blur = cv2.GaussianBlur(blur, (5, 5), 0)
-
-    if threshold_type:
-        blur = pipeline.threshold(blur, **{'method': threshold_type})['img']
-
-    if invert:
-        blur = ~blur
-
-    if dilate_iterations != 0:
-        blur = cv2.dilate(blur, np.ones((5, 5), np.uint8), iterations=dilate_iterations)
-    if erode_iterations != 0:
-        blur = cv2.erode(blur, np.ones((5, 5), np.uint8), iterations=erode_iterations)
-
-    if contour_type:
-        contours, _ = cv2.findContours(blur, cv2.RETR_LIST, contour_type)
-
-        if smoothness > 0:
-            arr = []
-            for cont in contours:
-                # TODO: add as argument
-                if cv2.arcLength(cont, False) > cleanliness:
-                    arr.append(cv2.approxPolyDP(cont, smoothness, False))
-            contours = arr
-
-        height, width = gray.shape
-        with open('staticFiles/images/main.svg', 'w+') as f:
-            svg = pipeline.contours2svg(contours, width, height)
-            f.write(
-                svg)  # f.write('<svg width="' + str(m) + '" height="' + str(n) + '" xmlns="http://www.w3.org/2000/svg">')  # for c in contours:  #     f.write('<path d="M')  #     for cords in c:  #         x, y = cords[0]  #         f.write(str(x) + ' ' + str(y) + ' ')  #     f.write('"/>')  # f.write('</svg>')
-
-    cv2.imwrite('./staticFiles/images/main.png', blur)
 
 
 def send_to_silhouette():
@@ -158,27 +82,157 @@ def send_to_silhouette():
                 path.append((x, y))
 
             scaled.append(path)
-
         return scaled
 
     with open('staticFiles/images/main.svg') as filename:
         contours = get_paths(filename)
         contours = scale(contours, max_width=100, max_height=100)
 
-        dev.setup(media=132, pen=True, pressure=10, speed=3)
-        dev.plot(pathlist=contours, offset=(5, 5))
+        # dev.setup(media=132, pen=True, pressure=10, speed=3)
+        # dev.plot(pathlist=contours, offset=(5, 5))
 
 
-def copy_backup():
-    img = cv2.imread('./staticFiles/images/backup.JPG')
-    cv2.imwrite('./staticFiles/images/main.png', img)
-    if os.path.exists('./staticFiles/images/main.svg'):
-        os.remove('./staticFiles/images/main.svg')
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if request.form.getlist('undo'):
+            undo()
+            return redirect(request.url)
+
+        if request.form.get('send-to-silhouette'):
+            send_to_silhouette()
+            return redirect(request.url)
+
+        blurs = request.form.getlist('blur') if request.form.getlist('blur') else None
+        threshold_type = request.form.get('threshold') if request.form.get('threshold') else None
+        invert = bool(request.form.get('invert')) if request.form.get('invert') else None
+        dilate_iterations = int(request.form.get('dilate')) if request.form.get('dilate') else None
+        erode_iterations = int(request.form.get('erode')) if request.form.get('erode') else None
+        contour_type = int(request.form.get('contour')) if request.form.get('contour') else None
+        cleanliness = float(request.form.get('clean')) / 10 if request.form.get('clean') else None
+        smoothness = float(request.form.get('smooth')) / 10 if request.form.get('smooth') else None
+
+        # check if the post request has the file part
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            # filename = secure_filename(file.filename)
+            delete_files_in_folder('./staticFiles/images/pngs/')
+            delete_files_in_folder('./staticFiles/images/svgs/')
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], '0.png'))
+            height, width = cv2.imread(UPLOAD_FOLDER + '0.png').shape[:2]
+            svg = f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg"></svg>'
+            write_svg(svg, 0)
+            return redirect(request.url)
+        root(blurs, threshold_type, invert, dilate_iterations, erode_iterations, contour_type, cleanliness, smoothness)
+        return redirect(request.url)
+    return render_template('index.html')
 
 
-# TODO: Add actual routes when needed
+def root(blurs, threshold_type: str, invert: bool, dilate_iterations: int, erode_iterations: int, contour_type: int,
+         cleanliness: float, smoothness: float):
+    idx = get_latest_img()
+    img = cv2.imread('./staticFiles/images/pngs/' + str(idx) + '.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    height, width = gray.shape
+    svg = f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg"></svg>'
+    blur = gray
+
+    if blurs:
+        if 'median' in blurs:
+            blur = cv2.medianBlur(blur, 3)
+        if 'gaussian' in blurs:
+            blur = cv2.GaussianBlur(blur, (5, 5), 0)
+
+    if threshold_type:
+        blur = pipeline.threshold(blur, **{'method': threshold_type})['img']
+
+    if invert:
+        blur = ~blur
+
+    if dilate_iterations != 0:
+        blur = cv2.dilate(blur, np.ones((5, 5), np.uint8), iterations=dilate_iterations)
+    if erode_iterations != 0:
+        blur = cv2.erode(blur, np.ones((5, 5), np.uint8), iterations=erode_iterations)
+
+    if contour_type:
+        contours, _ = cv2.findContours(blur, cv2.RETR_LIST, contour_type)
+
+        if smoothness > 0:
+            arr = []
+            for cont in contours:
+                # TODO: add as argument
+                if cv2.arcLength(cont, False) > cleanliness:
+                    arr.append(cv2.approxPolyDP(cont, smoothness, False))
+            contours = arr
+
+        svg = pipeline.contours2svg(contours, width, height)
+
+    add_img(blur, svg, idx)
+
+
+def get_latest_img():
+    for i in range(IMG_AMT)[::-1]:
+        path = './staticFiles/images/pngs/' + str(i) + '.png'
+        if os.path.exists(path):
+            return i
+
+
+def write_svg(svg, idx):
+    with open('staticFiles/images/svgs/' + str(idx) + '.svg', 'w+') as f:
+        f.write(svg)
+
+
+def add_img(png, svg, idx):
+    path = './staticFiles/images/pngs/'
+    if idx < IMG_AMT - 1:
+        cv2.imwrite(path + str(idx + 1) + '.png', png)
+        write_svg(svg, idx + 1)
+    else:
+        for i in range(1, IMG_AMT):
+            old_path = path + str(i) + '.png'
+            new_path = path + str(i - 1) + '.png'
+            if i == 1:
+                os.remove(new_path)
+                if os.path.exists('./staticFiles/images/svgs/0.svg'):
+                    os.remove('./staticFiles/images/svgs/0.svg')
+            os.rename(old_path, new_path)
+            old_svg_path = './staticFiles/images/svgs/' + str(i) + '.svg'
+            if os.path.exists(old_svg_path):
+                os.rename(old_svg_path, './staticFiles/images/svgs/' + str(i - 1) + '.svg')
+        cv2.imwrite(path + str(idx) + '.png', png)
+        write_svg(svg, idx)
+
+
+def undo():
+    idx = get_latest_img()
+    if idx > 0:
+        os.remove('./staticFiles/images/pngs/' + str(idx) + '.png')
+        svg_path = './staticFiles/images/svgs/' + str(idx) + '.svg'
+        if os.path.exists(svg_path):
+            os.remove(svg_path)
+
+
+def delete_files_in_folder(path):
+    all_files = os.listdir(path)
+    for f in all_files:
+        os.remove(path + f)
+
+
+def restart():
+    imgs = './staticFiles/images/'
+    pngs = imgs + 'pngs/'
+    svgs = imgs + 'svgs/'
+
+    delete_files_in_folder(pngs)
+    delete_files_in_folder(svgs)
+
+    img = cv2.imread(imgs + 'backup.JPG')
+    cv2.imwrite(pngs + '0.png', img)
+    height, width = img.shape[:2]
+    svg = f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg"></svg>'
+    write_svg(svg, 0)
 
 
 if __name__ == '__main__':
-    copy_backup()
+    restart()
     app.run(host=config['host'], port=config['port'])
